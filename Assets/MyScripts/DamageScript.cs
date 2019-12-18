8fd27617fbcb008dd7a77e3827f87bf09fb022e6
pwater44 <56414116+pwater44@users.noreply.github.com>
2019-12-18 17:59:34 -0500

    up to game manager

---
+++ Assets/MyScripts/DamageScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour
+{
+    [SerializeField] float damageAmount = 10;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        HealthScript health = collision.gameObject.GetComponent<HealthScript>();
+        if (health)
+        {
+            health.ApplyDamage(damageAmount);            
+        }
+    }
+}
